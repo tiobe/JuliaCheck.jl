61c6142370238717f37dd3c27e13a25e2e7a7fa2
Juanma Bellon <juan.bellon@tiobe.com>
2025-05-02 09:51:36 +0200

    Fixed output for clickable accurate links in console

--- src/Properties.jl
+++ src/Properties.jl
@@ -12,7 +12,7 @@ export opens_scope, closes_module, closes_scope, find_child_of_kind,
 
 function report_violation(node::SyntaxNode, severity::Int; user_msg::String, summary::String)
     line, column = JS.source_location(node)
-    printstyled("\n$(JS.filename(node)) ($line, $(column+1)):\n";
+    printstyled("\n$(JS.filename(node))($line, $(column)):\n";
                 underline=true)
     JS.highlight(stdout, node; note=user_msg, notecolor=:yellow,
                                context_lines_after=0, context_lines_before=0)
