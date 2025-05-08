49273b8dd119d0c7c085b205cd4a7a0719046582
Juanma Bellon <juan.bellon@tiobe.com>
2025-05-08 10:37:37 +0200

    Implemented "integration" tests (rules validation) (#36288)

---
+++ tests/single_module_file.jl
@@ -0,0 +1,15 @@
+module single_module_file
+
+module SubModuleIsOK
+end
+
+end     # module single_module_file
+
+module SecondModuleNotOK
+end
+
+function OutOfModuleIsWrong()
+end
+
+module ThirdInFile
+end
