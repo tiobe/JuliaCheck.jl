8903cd6ad5fffc9b704b1460309fba22a63b61a0
Juanma Bellon <juan.bellon@tiobe.com>
2025-05-08 11:16:09 +0200

    Implemented rule too_many_types_in_unions (#36348)

---
+++ checks/check_too_many_types_in_unions.jl
@@ -0,0 +1,17 @@
+module TooManyTypesInUnions
+
+import JuliaSyntax: SyntaxNode, @K_str, kind, children
+using ...Properties: is_union_decl, report_violation
+
+function check(union_decl::SyntaxNode)
+    @assert is_union_decl(union_decl) "Expected a Union declaration, got $(kind(union_decl))"
+    union_types = children(union_decl)[2:end] # discard the 1st, which is "Union"
+    if length(union_types) > 4
+        report_violation(union_decl; severity=6,
+                rule_id="asml-too-many-types-in-unions",
+                user_msg="Detected a union with too many types.",
+                summary="Too many types in Unions.")
+    end
+end
+
+end
--- src/Checks.jl
+++ src/Checks.jl
@@ -9,5 +9,6 @@ include("../checks/check_prefix_of_abstract_type_names.jl")
 include("../checks/check_single_module_file.jl")
 # include("../checks/check_space_around_infix_operators.jl")
 include("../checks/check_struct_members_are_in_lower_snake_case.jl")
+include("../checks/check_too_many_types_in_unions.jl")
 
 end
