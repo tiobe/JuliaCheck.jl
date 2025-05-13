e3b7a8f071d423020c684a48dabf32ad0ce8446e
Juanma Bellon <juan.bellon@tiobe.com>
2025-05-13 08:47:28 +0200

    Added test file

---
+++ tests/use_spaces_instead_of_tabs.jl
@@ -0,0 +1,13 @@
+function get_specific_numbers()::Vector{Int64}
+	vec = rand(Int64, 5)
+    # Get five random numbers between typemin(Int64) and typemax(Int64)
+
+    vec = sort([abs(element % 5) for element in vec])
+	# Of each element, take the absolute value, modulo 5, and sort
+
+    if isnothing(vec)
+    	println("Bad vector.	String are off-limits.")
+    end
+	
+    return vec
+end
