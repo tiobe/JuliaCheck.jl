f01d819aecc65d664933c81b98daaa5d33599196
Juanma Bellon <juan.bellon@tiobe.com>
2025-05-19 12:31:14 +0200

    Implemented rule implement_unions_as_const (#36423)

---
+++ checks/check_implement_unions_as_consts.jl
@@ -0,0 +1,21 @@
+module ImplementUnionsAsConsts
+
+import JuliaSyntax: SyntaxNode, @K_str, kind, children
+using ...Properties: is_assignment, is_constant, is_union_decl, report_violation
+
+function check(union::SyntaxNode)
+    @assert is_union_decl(union) "Expected a Union declaration, got $(kind(union))"
+    if is_assignment(union.parent) && is_constant(union.parent.parent)
+        # This seems to be a Union type declaration
+        if union == children(union.parent)[2]
+            # Confirmed. In this case, there is nothing to report.
+            return nothing
+        end
+    end
+    report_violation(union; severity=3,
+            rule_id="asml-implement-unions-as-consts",
+            user_msg="Declare this Union as a const type before using it.",
+            summary="Implement Unions as const.")
+end
+
+end
---
+++ tests/implement_unions_as_consts.jl
@@ -0,0 +1,42 @@
+module implement_unions_as_consts
+
+module BadStyle
+
+    struct MyStruct
+        member_x::Union{Nothing, Int64}
+        member_y::Union{Nothing, Int64}
+    end
+
+    get_member_x(my_struct::MyStruct)::Union{Nothing, Int64} = my_struct.member_x
+    get_member_y(my_struct::MyStruct)::Union{Nothing, Int64} = my_struct.member_y
+    function set_member_x(my_struct::MyStruct, value::Union{Nothing, Int64})::MyStruct
+        return MyStruct(value, my_struct.member_y)
+    end
+    function set_member_y(my_struct::MyStruct, value::Union{Nothing, Int64})::MyStruct
+        return MyStruct(my_struct.member_x, value)
+    end
+
+end
+
+module GoodStyle
+
+    """ Nullable integer."""
+    const MyUnion = Union{Nothing, Int64}
+
+    struct MyStruct
+        member_x::MyUnion
+        member_y::MyUnion
+    end
+
+    get_member_x(my_struct::MyStruct)::MyUnion = my_struct.member_x
+    get_member_y(my_struct::MyStruct)::MyUnion = my_struct.member_y
+    function set_member_x(my_struct::MyStruct, value::MyUnion)::MyStruct
+        return MyStruct(value, my_struct.member_y)
+    end
+    function set_member_y(my_struct::MyStruct, value::MyUnion)::MyStruct
+        return MyStruct(my_struct.member_x, value)
+    end
+
+end
+
+end
