1bba7e44fb6138d576314afed061848aaf98cfd1
Juanma Bellon <juan.bellon@tiobe.com>
2025-06-02 17:21:28 +0200

    RM-36412 [FP] 2nd part: fixed function_arguments_in_lower_snake_case

--- checks/check_function_arguments_in_lower_snake_case.jl
+++ checks/check_function_arguments_in_lower_snake_case.jl
@@ -1,13 +1,20 @@
 module FunctionArgumentsCasing
 
-import JuliaSyntax: SyntaxNode, @K_str, kind, children
+import JuliaSyntax: SyntaxNode, @K_str, kind, children, numchildren
 using ...Properties: find_first_of_kind, is_lower_snake, report_violation
 
 function check(f_name::SyntaxNode, f_arg::SyntaxNode)
     @assert kind(f_name) == K"Identifier" "Expected argument 'f_name' to be an [Identifier], not $(kind(f_name))"
-    f_arg = find_first_of_kind(K"Identifier", f_arg)
+
+    if kind(f_arg) == K"::"
+        f_arg = numchildren(f_arg) == 1 ? nothing : children(f_arg)[1]
+    end
+    if f_arg !== nothing
+        f_arg = find_first_of_kind(K"Identifier", f_arg)
+    end
     if isnothing(f_arg)
-        # Nothing to check; probably a semicolon followed by nothing at all (nasty)
+        # Nothing to check; maybe a ::Val or ::Type, or perhaps a semicolon
+        # followed by nothing at all (nasty)
         return nothing
     end
     arg_name = string(f_arg)
