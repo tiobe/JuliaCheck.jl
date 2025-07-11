dd32e4f981d6dab32726cace0a823f40a173f2f2
Juanma Bellon <juan.bellon@tiobe.com>
2025-07-11 16:07:10 +0200

    RM-36821: Implemented rule `location_of_global_variables`

---
+++ test/location_of_global_variables.jl
@@ -0,0 +1,38 @@
+module module_include_location
+
+module MyBadStylePackage
+
+    using Statistics
+
+    include("SomeSubmodule.jl")
+    import .SomeSubmodule
+
+    include("CoreModule.jl")
+
+    function foo()::Nothing
+        return nothing
+    end
+
+    const MY_GLOB = 42
+
+end # module MyBadStylePackage
+
+module MyGoodStylePackage
+
+    using Statistics
+
+    include("SomeOtherSubModule.jl")  # includes should be placed below the import/using list
+    import .SomeOtherSubModule
+
+    include("SomeSubmodule.jl")  # except `include` for a submodule
+    import .SomeSubmodule
+
+    const MY_GLOB = 42
+
+    function foo()::Nothing
+        return nothing
+    end
+
+end # module MyGoodStylePackage
+
+end # module module_include_location
