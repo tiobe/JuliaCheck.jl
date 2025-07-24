316ce8be89681243b7a835451d8579f35b159c5f
Juanma Bellon <juan.bellon@tiobe.com>
2025-07-24 14:31:50 +0200

    Adapted Scout.jl to the addition of lossless trees
    ... and improved it along the way.

--- src/Scout.jl
+++ src/Scout.jl
@@ -5,40 +5,26 @@ import JuliaSyntax: Kind, GreenNode, SyntaxNode, SourceFile, @K_str, @KSet_str,
 
 include("JuliaCheck.jl"); import .JuliaCheck: main
 using .JuliaCheck.Properties: SF, report_violation, _report_common
-
-SINK = IOBuffer()
+using .JuliaCheck.LosslessTrees: LosslessNode
 
 function JuliaCheck.Properties.report_violation(
         node::SyntaxNode; severity::Int, user_msg::String,
                           summary::String, rule_id::String
         )::Nothing
-    line, column = JS.source_location(node)
-    printstyled(SINK, "\n$(JS.filename(node))($line, $(column)):\n";
-                underline=true)
-    JS.highlight(SINK, node; note=user_msg, notecolor=:yellow,
-                               context_lines_after=0, context_lines_before=0)
-    _report_common(severity, rule_id, summary)
+    return nothing
+end
+function JuliaCheck.Properties.report_violation(
+        node::LosslessNode; delta::Int=0,
+                          severity::Int, user_msg::String,
+                          summary::String, rule_id::String)::Nothing
+    return nothing
 end
 function JuliaCheck.Properties.report_violation(
         ; index::Int, len::Int, line::Int, col::Int,
           severity::Int, user_msg::String, summary::String, rule_id::String
         )::Nothing
-    printstyled(SINK, "\n$(JS.filename(SF))($line, $col):\n";
-                underline=true)
-    JS.highlight(SINK, SF, index:index+len-1;
-                 note=user_msg, notecolor=:yellow,
-                 context_lines_after=0, context_lines_before=0)
-    _report_common(severity, rule_id, summary)
+    return nothing
 end
-function JuliaCheck.Properties._report_common(
-        severity::Int, rule_id::String, summary::String)::Nothing
-    printstyled(SINK, "\n$summary"; color=:cyan)
-    printstyled(SINK, "\nRule:"; underline=true)
-    printstyled(SINK, " $rule_id. ")
-    printstyled(SINK, "Severity:"; underline=true)
-    printstyled(SINK, " $severity\n")
-end
-
 
 if endswith(PROGRAM_FILE, "run_debugger.jl") || abspath(PROGRAM_FILE) == @__FILE__
     main(ARGS)
