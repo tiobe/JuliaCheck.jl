54d3acb140fef20bc8000be3391a1cc84e0dcba3
zon-tiobe <122348880+zon-tiobe@users.noreply.github.com>
2025-08-12 12:22:44 +0200

    Adapted DebugCheck.jl to run on the test file instead of the check itself.

--- .vscode/DebugCheck.jl
+++ .vscode/DebugCheck.jl
@@ -3,15 +3,19 @@ module DebugCheck
 import JuliaCheck
 
 """
-Runs JuliaCheck on the specified file, which is expected to be a check with corresponding rule id
+Runs JuliaCheck for a given rule on the testfile of that rule.
+The testfile shares the basename with the check, but is in the /test/ directory instead of the /checks/ directory.
+The rule shares the same name as the specified file, which is expected to be a check with corresponding rule id
 (Same as filename, with '-' instead of '_').
 This is used for debugging purposes in VSCode (to enable pressing F5 to debug when editing a check).
 """
 function Debug(filename::String)
+    testfile = realpath(joinpath(dirname(filename), "../test/", basename(filename)))
     rulename = replace(splitext(basename(filename))[1], "_" => "-")
-    JuliaCheck.main(["--verbose", "--enable", rulename, "--", filename])
+    JuliaCheck.main(["--verbose", "--enable", rulename, "--", testfile])
 end
 
 Debug(ARGS[1]) # This will be set by the launch.json configuration in VSCode
+
 
 end
