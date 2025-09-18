e6194e2c2d21216d8b8b7aa8272a2ad7a434a46d
zon-tiobe <paul.van.zon@tiobe.com>
2025-09-18 11:04:09 +0200

    Added one more test case inspired by code in JuliaCheck.

--- test/AvoidContainersWithAbstractTypes.jl
+++ test/AvoidContainersWithAbstractTypes.jl
@@ -4,6 +4,7 @@ another_vector = Array{Number}[]
 a_set = Set{AbstractFloat}([1.0, 2, 3])
 double_array = Array{Array{Number}}
 triple_array = Array{Array{Array{Number}}}
+const POWERS_OF_WRONG_TYPE = Set{Integer}([2^i for i in 1:20])
 
 # Good:
 num_vector = [1.0, 2, 3] # Type will be Vector{Float64}. Float64 is concrete.
@@ -12,3 +13,4 @@ another_vector = Array{Int128}[]
 a_set = Set{Float64}([1.0, 2, 3])
 double_array = Array{Array{Int64}}
 triple_array = Array{Array{Array{Int32}}}
+const POWERS_OF_TWO = Set{Int64}([2^i for i in 1:20])
