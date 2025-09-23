b0b5e38f29dab71aa5ad8329338c59176c7d89ed
zon-tiobe <paul.van.zon@tiobe.com>
2025-09-23 17:29:56 +0200

    Added one more line to the explanatory comment.

--- checks/VariablesHaveFixedTypes.jl
+++ checks/VariablesHaveFixedTypes.jl
@@ -16,6 +16,8 @@ eval calls or usage of Cthulhu (the Julia type inference package: see
 https://github.com/JuliaDebug/Cthulhu.jl) would be far too expensive or require us to
 check during runtime.
 
+The current state of this rule is very basic. Future work:
+
 If we want to check specific calls, the way to do it would be something like
 Base.return_types(getfield(Base, Symbol(function_name)))
 
