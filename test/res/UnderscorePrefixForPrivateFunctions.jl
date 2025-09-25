7259e858360d910b3ad95d9302ed32df6cd0110f
zon-tiobe <paul.van.zon@tiobe.com>
2025-09-25 16:33:52 +0200

    Added test cases for multiple import statements.

--- test/res/UnderscorePrefixForPrivateFunctions.jl
+++ test/res/UnderscorePrefixForPrivateFunctions.jl
@@ -1,6 +1,7 @@
 module IAmAnExportTestModule
 
-export i_am_exported_without_underscore, _i_am_exported_with_underscore, warblgarbl
+export i_am_exported_without_underscore, _i_am_exported_with_underscore
+
 
 function i_am_exported_without_underscore()::Bool
     return false
@@ -15,6 +16,16 @@ function _i_am_exported_with_underscore()::Bool
 end
 
 function _i_am_not_exported_with_underscore()::Bool
+    return false
+end
+
+export second_export_is_also_flagged, _even_if_underscored
+
+function second_export_is_also_flagged()::Bool
+    return false
+end
+
+function _even_if_underscored()::Bool
     return false
 end
 
