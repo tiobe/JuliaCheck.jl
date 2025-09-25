2e3f788af2ac81429237b838417d68a1135d56fb
zon-tiobe <paul.van.zon@tiobe.com>
2025-09-25 15:59:04 +0200

    Added explanation of rule limitations.

--- checks/UnderscorePrefixForPrivateFunctions.jl
+++ checks/UnderscorePrefixForPrivateFunctions.jl
@@ -9,6 +9,31 @@ id(::Check) = "underscore-prefix-for-private-functions"
 severity(::Check) = 8
 synopsis(::Check) = "Private functions are prefixed with one underscore _ character."
 
+#=
+There seem to be two attitudes with regard to writing Julia modules.
+
+One is to have one-module-per-file in the same way as we do here. If it's one-module-per-file,
+this rule works just fine as we have all data we need in the tree (since if there's only one
+file per module, the export statement is guaranteed to be findable).
+
+Another is the typical idiom as can be seen in JuliaSyntax:
+
+module X 
+
+include("file_a.jl")
+include("file_b.jl")
+
+export fn_file_a, fn_file_b
+
+end
+
+In that case this rule won't help as currently it is not the intent to check the entire project
+(or at least, to track in _which_ module a given source file actually resides). As such, if we
+want to support the second case then it's not coverable as of yet. This rule will only be
+triggered on a module statement. If there is no module to be found in a file (implying it's either
+a script or a part of a different module) this rule will not trigger.
+=#
+
 function init(this::Check, ctxt::AnalysisContext)
     register_syntaxnode_action(ctxt, is_toplevel, n -> _check(this, ctxt, n))
     return nothing
@@ -22,11 +47,11 @@ function _check(this::Check, ctxt::AnalysisContext, toplevel_node::SyntaxNode)
             function_name = string(function_name_node)
             has_underscore = startswith(function_name, "_")
             if has_underscore && function_name ∈ all_exported_names
-                report_violation(ctxt, this, function_node,
+                report_violation(ctxt, this, function_name_node,
                     "Function $(function_name) is exported while having a name starting with an underscore.")
             end
             if !has_underscore && function_name ∉ all_exported_names
-                report_violation(ctxt, this, function_node,
+                report_violation(ctxt, this, function_name_node,
                     "Function $(function_name) is not exported while having a name starting without an underscore.")
             end
         end
