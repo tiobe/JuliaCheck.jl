de729a5665d5407a6b2c95d234f496565f2d26d2
zon-tiobe <paul.van.zon@tiobe.com>
2025-09-25 11:03:52 +0200

    Initial patchy implementation of UnderscorePrefixForPrivateFunctions. Potentially still a lot to improve.

---
+++ checks/UnderscorePrefixForPrivateFunctions.jl
@@ -0,0 +1,50 @@
+module UnderscorePrefixForPrivateFunctions
+
+include("_common.jl")
+
+using ...Properties: get_func_name, is_export, is_function, is_toplevel
+
+struct Check<:Analysis.Check end
+id(::Check) = "underscore-prefix-for-private-functions"
+severity(::Check) = 8
+synopsis(::Check) = "Private functions are prefixed with one underscore _ character."
+
+function init(this::Check, ctxt::AnalysisContext)
+    register_syntaxnode_action(ctxt, is_toplevel, n -> _check(this, ctxt, n))
+    return nothing
+end
+
+function _check(this::Check, ctxt::AnalysisContext, toplevel_node::SyntaxNode)
+    all_exported_names = _get_exported_function_names(toplevel_node)
+    for child_node in children(toplevel_node)
+        if is_function(child_node)
+            function_name_node = get_func_name(child_node)
+            if !isnothing(function_name_node)
+                function_name = string(function_name_node)
+                has_underscore = startswith(function_name, "_")
+                if has_underscore && function_name ∈ all_exported_names
+                    report_violation(ctxt, this, child_node,
+                        "Function $(function_name) is exported while having a name starting with an underscore.")
+                end
+                if !has_underscore && function_name ∉ all_exported_names
+                    report_violation(ctxt, this, child_node,
+                        "Function $(function_name) is not exported while having a name starting without an underscore.")
+                end
+            end
+        end
+    end
+end
+
+function _get_exported_function_names(toplevel_node::SyntaxNode)::Set{String}
+    exported_names = Set{String}()
+    for child_node in children(toplevel_node)
+        if is_export(child_node)
+            for exported in children(child_node)
+                push!(exported_names, string(exported))
+            end
+        end
+    end
+    return exported_names
+end
+
+end # module UseEachindexToIterateIndices
---
+++ test/res/UnderscorePrefixForPrivateFunctions.jl
@@ -0,0 +1,17 @@
+export i_am_exported_without_underscore, _i_am_exported_with_underscore, warblgarbl
+
+function i_am_exported_without_underscore()::Bool
+    return false
+end
+
+function i_am_not_exported_without_underscore()::Bool
+    return false
+end
+
+function _i_am_exported_with_underscore()::Bool
+    return false
+end
+
+function _i_am_not_exported_with_underscore()::Bool
+    return false
+end
