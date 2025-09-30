93873f16eef03b28c9f068d56acc4986c3278da7
brouwers <122355335+brouwers-tiobe@users.noreply.github.com>
2025-09-30 09:57:00 +0200

    Extend test cases

--- test/res/MultilineCommentsForManyLines.jl
+++ test/res/MultilineCommentsForManyLines.jl
@@ -30,3 +30,22 @@ end of comment =#
 #= another multiline x2 =#
 #= another multiline x3 =#
 #= another multiline x4 =#
+"""
+This docstring is long, in order to provide lots oḟ context
+and explanation for the function.
+
+It does things, then reports on the things we did.
+If no things were done, the report will be empty.
+
+As docstrings are not comments, this is okay and no violation is reported.
+"""
+function doThings()::String
+    return """
+    I have done all the things,
+    including writing this long string
+    which I am reporting now.
+    This should not become a multiline comment.
+    There are lots of interesting findings here,
+    from doing the thing
+    """
+end
