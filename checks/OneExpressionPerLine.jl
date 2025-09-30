592395075e51f9082948f1f7e2994d0c75d606f0
zon-tiobe <paul.van.zon@tiobe.com>
2025-09-30 12:10:31 +0200

    Extended rule OneExpressionPerLine with more checks.

--- checks/OneExpressionPerLine.jl
+++ checks/OneExpressionPerLine.jl
@@ -1,7 +1,7 @@
 module OneExpressionPerLine
 
-using JuliaSyntax: has_flags, JuliaSyntax as JS
-using ...Properties: is_toplevel
+using JuliaSyntax: has_flags, is_leaf, JuliaSyntax as JS
+using ...Properties: is_assignment, is_toplevel
 
 include("_common.jl")
 
@@ -10,16 +10,41 @@ id(::Check) = "one-expression-per-line"
 severity(::Check) = 7
 synopsis(::Check) = "The number of expressions per line is limited to one."
 
-function init(this::Check, ctxt::AnalysisContext)
-    register_syntaxnode_action(ctxt, _is_toplevel_semicolon, n -> check(this, ctxt, n))
+function init(this::Check, ctxt::AnalysisContext)::Nothing
+    register_syntaxnode_action(ctxt, _is_toplevel_semicolon, n -> _check(this, ctxt, n))
+    return nothing
 end
 
+"""
+JuliaSyntax parses semicolon-concatenated statements to a toplevel-; node.
+
+To distinguish the semicolon-separated statement, it's necessary to check against the
+specific flag, which (unfortunately) is not nicely exposed like other flags and needs
+to have its own explicit check.
+"""
 function _is_toplevel_semicolon(node)::Bool
     return is_toplevel(node) && has_flags(node, JS.TOPLEVEL_SEMICOLONS_FLAG) 
 end
 
-function check(this::Check, ctxt::AnalysisContext, node::SyntaxNode)
-    report_violation(ctxt, this, node, "Do not concatenate statements with a semicolon.")
+"""
+Checks whether a given toplevel-; node contains multiple statements.
+
+The one case that this rule is meant to exclude is code that's written like this:
+x = 6;
+x + 2;
+
+While not really nice Julia, it's valid Julia, and might be a common mistake if the
+writer of the code is used to a C-style language and habitually postfixes every
+statement with a semicolon.
+"""
+function _check(this::Check, ctxt::AnalysisContext, node::SyntaxNode)::Nothing
+    if is_leaf(node)
+        return nothing
+    end
+    if length(children(node)) > 1 || !is_assignment(first(children(node)))
+        report_violation(ctxt, this, node, "Do not concatenate statements with a semicolon.")
+    end
+    return nothing
 end
 
 end # module OneExpressionPerLine
--- test/res/OneExpressionPerLine.jl
+++ test/res/OneExpressionPerLine.jl
@@ -1,6 +1,8 @@
 # Bad style:
 for bar::Int64 in range(1, 3); println("bar ", bar); if bar == 2 println("bar equals two"); end; end;
 
+x = 4; x + 5
+
 # Good style:
 for bar::Int64 in range(1, 3)
     println("bar ", bar)
@@ -9,3 +11,18 @@ for bar::Int64 in range(1, 3)
         println("bar equals two, but correct now")
     end
 end
+
+# Ignore semicolons that are not really statement separators
+for i in [1:3];
+    println("i $i type ", typeof(i))
+end
+
+# Ignore semicolons in vcats
+vcat_struct = [
+  1 2;
+  3 4
+]
+
+# Well, not really good, but it shouldn't throw a violation.
+y = 5;
+y + 6
