8687567ae83171a5c7999d8fa675aac5f8c87c8c
brouwers <122355335+brouwers-tiobe@users.noreply.github.com>
2025-10-02 11:58:32 +0200

    Implement rule UseAmericanEnglish

---
+++ checks/UseAmericanEnglish.jl
@@ -0,0 +1,57 @@
+module UseAmericanEnglish
+
+using JuliaSyntax: byte_range, view, GreenNode, SyntaxNode, SourceFile, JuliaSyntax as JS
+using ...Properties: is_toplevel
+using ...CommentHelpers: get_comments, get_text, get_range, contains_comments
+
+include("_common.jl")
+
+struct Check<:Analysis.Check end
+id(::Check) = "use-american-english"
+severity(::Check) = 9
+synopsis(::Check) = "Comments should be in the American-English language"
+
+function _load_words(path::String)::Vector{AbstractString}
+    include_dependency(path)
+    return open(path) do f
+        return readlines(f)
+    end
+end
+
+function init(this::Check, ctxt::AnalysisContext)::Nothing
+    forbidden_words = _load_words(joinpath(@__DIR__, "_config", "words_en-GB.txt"))
+    register_syntaxnode_action(ctxt, contains_comments, n -> _check_comment(this, ctxt, n, forbidden_words))
+    register_syntaxnode_action(ctxt, n -> kind(n) == K"doc", n -> _check_docstring(this, ctxt, n, forbidden_words))
+    return nothing
+end
+
+function _check_docstring(this::Check, ctxt::AnalysisContext, node::SyntaxNode, forbidden_words::Vector{AbstractString})::Nothing
+    string_node = children(node)[1]
+    text = sourcetext(string_node)
+    _check_for_british_spellings(this, ctxt, forbidden_words, text, byte_range(node))
+    return nothing
+end
+
+function _check_comment(this::Check, ctxt::AnalysisContext, node::SyntaxNode, forbidden_words::Vector{AbstractString})::Nothing
+    comments = get_comments(node)
+    for c in comments
+        _check_for_british_spellings(this, ctxt, forbidden_words, get_text(c), get_range(c))
+    end
+    return nothing
+end
+
+function _check_for_british_spellings(this::Check, ctxt::AnalysisContext, forbidden_words::Vector{AbstractString}, text::AbstractString, range::UnitRange)::Nothing
+    forbidden_words_in_text = _contained_words_from(forbidden_words, text)
+    if !isempty(forbidden_words_in_text)
+        report_violation(ctxt, this, range, "Text contains British spelling: $(join(forbidden_words_in_text, ", ")).")
+    end
+end
+
+_comment_or_docstring(node::GreenNode) = kind(node) in KSet"Comment"
+
+function _contained_words_from(forbidden_words::Vector{AbstractString}, text::AbstractString)::Vector{AbstractString}
+    words = map(m -> m.match, eachmatch(r"\w+", lowercase(text)))
+    return filter(in(words), forbidden_words)
+end
+
+end # module UseAmericanEnglish
---
+++ test/res/UseAmericanEnglish.jl
@@ -0,0 +1,12 @@
+"""
+Initialise the variables to the appropriate colours, i.e. grey in light mode or black in dark mode
+"""
+function init()
+#=
+If running in a virtualised environment, make sure the blabla is bla
+=#
+
+    #=
+    If already initialized, do not re-initialize
+    =#
+end
