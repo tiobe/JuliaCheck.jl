d1a1c11cc2c86446f7680e783f2225a22e1fc2d6
reniers-tiobe <dennie.reniers@tiobe.com>
2025-10-13 08:43:19 +0200

    Add test case to golden input file for RM-37281

--- test/res/AvoidExtraneousWhitespaceBetweenOpenAndCloseCharacters.jl
+++ test/res/AvoidExtraneousWhitespaceBetweenOpenAndCloseCharacters.jl
@@ -26,9 +26,9 @@ tuple = (a, b, c)
 block = (a; b; c)
 
 # RM-37280: Avoid false positives
-struct FresnelCoefficientContext
-    ringWidth::Float64 = 0.003                       #width of ring
-    ringCenter::Float64 = 0.145                      #center of ring
+struct Context
+    width::Float64 = 1.000                       #width
+    center::Float64 = 1.000                      #center
 end
 hints = ([0, 1,
         60,                 # minutes, seconds
@@ -38,3 +38,11 @@ const KNOWN_FLOATS = Set{Float64}([0.1, 0.01, 0.001, 0.0001, 0.5]) ∪
                     Set{Float64}(convert.(Float64, POWERS_OF_TEN))
 if kind(glob_var) == K"="      glob_var = first_child(glob_var) end
 @assert true  "a" * "b"
+
+# RM-37281: Avoid false positives
+m_vec .-= 1  # Rows and columns as returned by _vector_index_to_matrix_row_col!
+function Base.isapprox(context1::Context, context2::Context; kwargs...)
+    equal::Bool = context1.a           == context2.a          &&
+                  context1.m           == context2.m               &&
+                  context1.n           == context2.n
+end
