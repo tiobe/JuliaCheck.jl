932a3289f0ad4e5e4671ee9ad30e0d7641a13f73
reniers-tiobe <dennie.reniers@tiobe.com>
2026-01-20 16:02:13 +0100

    RM-37344: report use-eachindex-to-iterate-indices only once
    Improved the implementation of the rule as follows:
    
    - We report a violation for `use-eachindex-to-iterate-indices` exactly once for a loop variable when that variable is used as an array index (`ref`).
    - We report the violation on the variable that is actually used as an array index, instead of reporting on the closest parent for-loop.

--- checks/UseEachindexToIterateIndices.jl
+++ checks/UseEachindexToIterateIndices.jl
@@ -2,33 +2,55 @@ module UseEachindexToIterateIndices
 
 include("_common.jl")
 
-using ...Properties: get_iteration_parts, is_loop, is_range, is_stop_point
+using ...Properties: get_iteration_parts, NullableNode, is_range, is_stop_point
 
-struct Check<:Analysis.Check end
+struct Check<:Analysis.Check
+    already_reported::Set{SyntaxNode}
+    Check() = new(Set{SyntaxNode}())
+end
 id(::Check) = "use-eachindex-to-iterate-indices"
 severity(::Check) = 5
 synopsis(::Check) = "Use eachindex() instead of a constructed range for iteration over a collection."
 
 function init(this::Check, ctxt::AnalysisContext)
-    register_syntaxnode_action(ctxt, n -> kind(n) == K"ref", node -> check(this, ctxt, node))
+    register_syntaxnode_action(ctxt, n -> kind(n) == K"ref", node -> _check(this, ctxt, node))
 end
 
-function check(this::Check, ctxt::AnalysisContext, index_ref::SyntaxNode)
+"""
+Given a array index syntax node `ref`,
+this function searches upwards through the parent nodes to find an enclosing `for` loop
+where the variable is defined as a loop iterator.
+"""
+function _find_enclosing_loop_binding(ref::SyntaxNode)::Tuple{NullableNode, NullableNode}
+    if length(ref.children) == 2 && kind(ref.children[2]) == K"Identifier"
+        ref_var = string(ref.children[2])
+    else
+        return (nothing, nothing)
+    end
+    n = ref;
+    while !isnothing(n) && !is_stop_point(n)
+        if kind(n) == K"for"
+            loop_var, loop_expr = get_iteration_parts(n)
+            if string(loop_var) == ref_var
+                return (loop_var, loop_expr)
+            end
+        end
+        n = n.parent
+    end
+    return (nothing, nothing)
+end
+
+function _check(this::Check, ctxt::AnalysisContext, index_ref::SyntaxNode)::Nothing
     @assert kind(index_ref) == K"ref" "Expected a [ref] node, got $(kind(index_ref))."
 
-    for_loop = index_ref.parent
-    while !(isnothing(for_loop) || is_loop(for_loop) || is_stop_point(for_loop))
-        for_loop = for_loop.parent
-    end
-    if isnothing(for_loop) || kind(for_loop) != K"for"
-        # Did not find a [for] loop containing the array indexing.
-        return nothing
-    end
-
-    loop_var, loop_expr = get_iteration_parts(for_loop)
+    (loop_var, loop_expr) = _find_enclosing_loop_binding(index_ref)
     if !isnothing(loop_var) && is_range(loop_expr)
-        report_violation(ctxt, this, loop_var, synopsis(this))
+        if loop_var ∉ this.already_reported
+            push!(this.already_reported, loop_var)
+            report_violation(ctxt, this, loop_var, synopsis(this))
+        end
     end
+    return nothing
 end
 
 end # module UseEachindexToIterateIndices
--- test/res/UseEachindexToIterateIndices.jl
+++ test/res/UseEachindexToIterateIndices.jl
@@ -1,23 +1,39 @@
-weights_vec = [2.2, 1.2, 3.4, 4.5]
+weights_vec = [2.2, 1.2, 3.4, 4.5, 5.6]
 
 # Bad style:
-for index in 1:5 # This raises a BoundsError
+for index in 1:5 # One violation on `index`
+    println("index $index weight $(weights_vec[index])")
+    b = weights_vec[index] # Violation should only be reported once
+end
+
+for index in 1:5 # Violation on `index`
+    while false # while-loop should not affect the violation
+        a = weights_vec[index]
+    end
+end
+
+for index in range(1, length(weights_vec)) # Violation on `index`
     println("index $index weight $(weights_vec[index])")
 end
 
-for index in range(1:length(weights_vec))
-    println("index $index weight $(weights_vec[index])")
+for i1 in 1:2 # Violation on `i1`
+    for i2 in 1:2 # Violation on `i2`
+        println(weights_vec[i1] + weights_vec[i2])
+    end
 end
 
 # Good style:
 for index in eachindex(weights_vec)
-    println("index ", index, " weight $(weights_vec[index])")
+    for i in 1:5 # For-loop with range, but is not used an array index
+        println("index ", i, " weight $(weights_vec[index])")
+    end
 end
 
 for weight in weights_vec
     println("weight $weight")
 end
 
-for i in 1:10
+for i in 1:2
     println("i = $i")
+    println(weights_vec[i*2]) # Loop variable used in an expression should be ignored
 end
