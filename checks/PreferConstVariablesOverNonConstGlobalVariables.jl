cbe5e2565f9ede28f38212fe71f7b5ebe4acc5bd
brouwers <122355335+brouwers-tiobe@users.noreply.github.com>
2026-01-21 09:26:22 +0100

    Fix FN and FP in 'Prefer const over non-const global' rule (#104)
    * Do not report if const cannot be added
    
    Rule `prefer-const-variables-over-non-const-global-variables` no longer
    triggers in the following cases, as `const` is not allowed there:
    * Global variables accessed in non-toplevel scopes, even if they have a
    'global' annotation (const keyword is not allowed here)
    * Variable declarations without a value assignment (e.g.
    `SOME_NUMBER::Float64`)
    
    * Report on every variable in multi-var assignment
    
    Also:
    * add single quotes around variable name in message
    * use `isnothing` instead of `!== nothing`

--- checks/PreferConstVariablesOverNonConstGlobalVariables.jl
+++ checks/PreferConstVariablesOverNonConstGlobalVariables.jl
@@ -2,7 +2,7 @@ module PreferConstVariablesOverNonConstGlobalVariables
 
 include("_common.jl")
 
-using ...Properties: find_lhs_of_kind, is_assignment
+using ...Properties: find_lhs_of_kind, is_assignment, is_global_decl, get_all_assignees
 using ...SyntaxNodeHelpers
 
 struct Check<:Analysis.Check end
@@ -11,23 +11,19 @@ severity(::Check) = 3
 synopsis(::Check) = "Prefer const variables over non-const global variables"
 
 function init(this::Check, ctxt::AnalysisContext)
-    register_syntaxnode_action(ctxt, is_assignment, node -> begin
-
+    register_syntaxnode_action(ctxt, n -> is_global_decl(n) && is_assignment(n), node -> begin
         ancs = ancestors(node)
         head = ancs[1:something(findfirst(is_scope_construct, ancs), length(ancs))]
         enclosing_scope = head[end]
-        if (!(kind(enclosing_scope) in KSet"toplevel module baremodule")
-            && !any(n -> kind(n) == K"global", head))
-            # Skip assignment that is not in global scope and does not have an explicit global modifier
+        if any(n -> kind(n) == K"const", head)
+            # Skip assignment to const variables
             return
         end
-        if any(n -> kind(n) in KSet"const local", head)
-            # Skip assignment to const or local variables
-            return
-        end
-        glob_var = find_lhs_of_kind(K"Identifier", node)
-        if glob_var !== nothing
-            report_violation(ctxt, this, glob_var, "Consider making global variable $glob_var a const.")
+        glob_vars = get_all_assignees(node)
+        for glob_var in glob_vars
+            if !isnothing(glob_var)
+                report_violation(ctxt, this, glob_var, "Consider making global variable '$glob_var' a const.")
+            end
         end
     end)
 end
