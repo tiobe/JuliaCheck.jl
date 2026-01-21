8581ffa55553efb12199bc158a9adf22d9a0f998
Dennie Reniers <121094479+reniers-tiobe@users.noreply.github.com>
2026-01-21 11:18:47 +0100

    RM-37345: Fix FP on comparing to Nothing (#106)
    Rule `use-isnothing-to-check-for-nothing-values` now only triggers on comparison with `nothing`, not `Nothing`.

--- checks/UseIsnothingToCheckForNothingValues.jl
+++ checks/UseIsnothingToCheckForNothingValues.jl
@@ -3,21 +3,22 @@ module UseIsnothingToCheckForNothingValues
 include("_common.jl")
 
 using ...Properties: is_eq_neq_comparison
-using ...SyntaxNodeHelpers
+using ...SyntaxNodeHelpers: apply_to_operands, extract_special_value
 
 struct Check<:Analysis.Check end
 id(::Check) = "use-isnothing-to-check-for-nothing-values"
 severity(::Check) = 3
 synopsis(::Check) = "Use isnothing to check variables for nothing"
 
-function init(this::Check, ctxt::AnalysisContext)
+function init(this::Check, ctxt::AnalysisContext)::Nothing
     register_syntaxnode_action(ctxt, is_eq_neq_comparison, node -> begin
         apply_to_operands(node, node -> begin
-            if extract_special_value(node) ∈ SyntaxNodeHelpers.NOTHING_VALUES
+            if extract_special_value(node) == "nothing"
                 report_violation(ctxt, this, node, synopsis(this))
             end
         end)
     end)
+    return nothing
 end
 
 end # module UseIsnothingToCheckForNothingValues
