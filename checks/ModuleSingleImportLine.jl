5620f3eaa50a905bef5dece962c7c4524589ccc1
zon-tiobe <122348880+zon-tiobe@users.noreply.github.com>
2026-01-22 09:54:59 +0100

    Only one violation on wrong ordering of a include/using list (#107)
    * Reworked ModuleSingleImportLine to better reflect the different internal changes.
    
    * Changed modjule to the clearer module_node.
    
    * Removed spurious print statement.
    
    * No longer reporting on multiline imports for ordering.
    
    * Ensured that violation reporting on module ordering stops after finding one that is ordered wrongly.
    
    * Added extra test for includes as well as usings.
    
    * Fixed expected results.
    
    * Fixed ordering.
    
    * Improved spelling.
    
    * Refactored filtering to work in a clearer way.
    
    * Added extra testcase for testing multi-import after regular imports.

--- checks/ModuleSingleImportLine.jl
+++ checks/ModuleSingleImportLine.jl
@@ -6,45 +6,58 @@ using ...Properties: get_imported_pkg, is_import, is_include, is_module
 struct Check<:Analysis.Check end
 id(::Check) = "module-single-import-line"
 severity(::Check) = 9
-synopsis(::Check) = "The list of packages should be in alphabetic order"
+synopsis(::Check) = "The list of packages should be in alphabetical order"
 
 function init(this::Check, ctxt::AnalysisContext)
     register_syntaxnode_action(ctxt, is_module, n -> check(this, ctxt, n))
 end
 
+function check(this::Check, ctxt::AnalysisContext, module_node::SyntaxNode)::Nothing
+    @assert kind(module_node) == K"module" "Expected a [module] node, got [$(kind(module_node))]."
+    @assert numchildren(module_node) == 2 "This module has a weird shape: "* string(module_node)
+    @assert kind(children(module_node)[2]) == K"block" "The second child of a [module] node is not a [block]!"
 
-function check(this::Check, ctxt::AnalysisContext, modjule::SyntaxNode)::Nothing
-    @assert kind(modjule) == K"module" "Expected a [module] node, got [$(kind(modjule))]."
-    @assert numchildren(modjule) == 2 "This module has a weird shape: "* string(modjule)
-    @assert kind(children(modjule)[2]) == K"block" "The second child of a [module] node is not a [block]!"
+    # Filters on using, import, include.
+    imports = filter(is_import, children(children(module_node)[2]))
 
-    imports = filter(is_import, children(children(modjule)[2]))
-    first_include = findfirst(is_include, imports)
-    if isnothing(first_include) first_include = 1 + length(imports) end
+    _check_multiple_imports_on_line(this, ctxt, imports)
+    _check_import_ordering(this, ctxt, imports)
+    _check_include_ordering(this, ctxt, imports)
+    return nothing
+end
 
-    # First, check import's and using's (they are supposed to be contiguous, or
-    # else they violate another rule)
-    previous = ""
-    for node in imports[1 : first_include-1]
+function _check_multiple_imports_on_line(this::Check, ctxt::AnalysisContext, imports::Vector{SyntaxNode})::Nothing
+    for node in filter(!is_include, imports)
         if numchildren(node) > 1
             report_violation(ctxt, this, node, "Import only one package per line.")
-        else
-            pkg_name = get_imported_pkg(node)
+        end
+    end
+    return nothing
+end
+
+function _check_import_ordering(this::Check, ctxt::AnalysisContext, imports::Vector{SyntaxNode})::Nothing
+    previous = ""
+    for node in filter(!is_include, imports)
+        pkg_name = get_imported_pkg(node)
+        if numchildren(node) <= 1
             if pkg_name < previous
                 report_violation(ctxt, this, node, synopsis(this))
+                return nothing
             else
                 previous = pkg_name
             end
         end
     end
+    return nothing
+end
 
-    # Now, we check the include's. Only their sorting (cannot include multiple
-    # files at once)
+function _check_include_ordering(this::Check, ctxt::AnalysisContext, imports::Vector{SyntaxNode})::Nothing
     previous = ""
-    for node in filter(is_include, imports[first_include : end])
+    for node in filter(is_include, imports)
         pkg_name = get_imported_pkg(node)
         if pkg_name < previous
             report_violation(ctxt, this, node, synopsis(this))
+            return nothing
         else
             previous = pkg_name
         end
@@ -53,4 +66,3 @@ function check(this::Check, ctxt::AnalysisContext, modjule::SyntaxNode)::Nothing
 end
 
 end # module ModuleSingleImportLine
-
