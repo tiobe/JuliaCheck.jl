05c696c6ffa0a7efe5db83b5d1300c8c486b7446
brouwers <122355335+brouwers-tiobe@users.noreply.github.com>
2026-01-22 12:17:50 +0100

    Do not report `prefer-const-variables-over-non-const-global-variables` on mutations
    Field assignments `A.x = 1` and array element assignments `A[i] = 2` now
    are no longer reported

--- checks/PreferConstVariablesOverNonConstGlobalVariables.jl
+++ checks/PreferConstVariablesOverNonConstGlobalVariables.jl
@@ -3,29 +3,32 @@ module PreferConstVariablesOverNonConstGlobalVariables
 include("_common.jl")
 
 using ...Properties: is_assignment, is_global_decl
-using ...SyntaxNodeHelpers: ancestors, get_all_assignees, is_scope_construct
+using ...SyntaxNodeHelpers: get_all_assignees, ancestors, is_scope_construct
 
 struct Check<:Analysis.Check end
 id(::Check) = "prefer-const-variables-over-non-const-global-variables"
 severity(::Check) = 3
 synopsis(::Check) = "Prefer const variables over non-const global variables"
 
-function init(this::Check, ctxt::AnalysisContext)
+function init(this::Check, ctxt::AnalysisContext)::Nothing
     register_syntaxnode_action(ctxt, n -> is_global_decl(n) && is_assignment(n), node -> begin
-        ancs = ancestors(node)
-        head = ancs[1:something(findfirst(is_scope_construct, ancs), length(ancs))]
-        enclosing_scope = head[end]
-        if any(n -> kind(n) == K"const", head)
-            # Skip assignment to const variables
-            return
+        lhs = first(children(node))
+        if kind(lhs) ∈ KSet". ref" # Exclude mutation (field assignment `A.x = 1` or array mutation `A[i] = 1`)
+            return nothing
         end
         glob_vars = get_all_assignees(node)
         for glob_var in glob_vars
-            if !isnothing(glob_var)
+            if !isnothing(glob_var) && !has_const_annotation(glob_var)
                 report_violation(ctxt, this, glob_var, "Consider making global variable '$glob_var' a const.")
             end
         end
     end)
+end
+
+function has_const_annotation(node::SyntaxNode)
+    ancs = ancestors(node)
+    head = ancs[1:something(findfirst(is_scope_construct, ancs), length(ancs))]
+    return any(n -> kind(n) == K"const", head)
 end
 
 end # module PreferConstVariablesOverNonConstGlobalVariables
