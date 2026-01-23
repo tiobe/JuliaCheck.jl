172bc107e9fd8785f548bb7f4b5b41d58e5de185
zon-tiobe <122348880+zon-tiobe@users.noreply.github.com>
2026-01-23 12:47:05 +0100

    Remove check on code outside modules (#110)
    * Removed check on code outside modules.
    * Added extra tests.
    * Adapted expected test results.

--- checks/SingleModuleFile.jl
+++ checks/SingleModuleFile.jl
@@ -13,14 +13,14 @@ function init(this::Check, ctxt::AnalysisContext)
     register_syntaxnode_action(ctxt, is_module, node -> check(this, ctxt, node))
 end
 
-function check(this::Check, ctxt::AnalysisContext, modjule::SyntaxNode)
-    @assert kind(modjule) == K"module" "Expected a [module] node, got [$(kind(node))]."
-    father = modjule.parent
+function check(this::Check, ctxt::AnalysisContext, module_node::SyntaxNode)
+    @assert kind(module_node) == K"module" "Expected a [module] node, got [$(kind(node))]."
+    father = module_node.parent
     kids = children(father)
     if kind(father) == K"toplevel"
-        mod_id = children(modjule)[1]
+        mod_id = children(module_node)[1]
         mod_name = string(mod_id)
-        file_name = basename(filename(modjule))[1:end-3]
+        file_name = basename(filename(module_node))[1:end-3]
         if mod_name !== file_name
             report_violation(ctxt, this, mod_id,
                     "Module name $mod_id should match its file name: $file_name."
@@ -30,7 +30,7 @@ function check(this::Check, ctxt::AnalysisContext, modjule::SyntaxNode)
             # This is not a submodule, and it has got siblings, which is bad, since
             # the top-level module should contain the whole of the file's contents.
             modules = filter(is_module, kids)
-            if modjule !== modules[1]
+            if module_node !== modules[1]
                 # Everything else below has already been reported
                 return nothing
             end
@@ -42,9 +42,6 @@ function check(this::Check, ctxt::AnalysisContext, modjule::SyntaxNode)
                 report_violation(ctxt, this, mod_id,
                         "Module '$mod_name' should be inside '$name_1st_mod' or in its own file."
                         )
-            end
-            for node in kids[kids .∉ Ref(modules)]
-                report_violation(ctxt, this, node, "Move this code into module '$name_1st_mod'.")
             end
         end
     end
---
+++ test/res/SingleModuleFile.jl/SingleModuleOk.jl
@@ -0,0 +1,11 @@
+module SingleModuleOk
+using ImportA
+using ImportB
+using ImportC
+using ImportD
+using ImportE
+using ImportF
+using ImportG
+end
+
+const SOMETHING_NEW = 100
