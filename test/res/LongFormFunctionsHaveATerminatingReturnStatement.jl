11316ee79015978f6030714ef0b9b22115fc9c00
reniers-tiobe <dennie.reniers@tiobe.com>
2026-01-23 10:22:13 +0100

    Fix FP on `long-form-functions-have-a-terminating-return-statement` for if-elsif-else

--- test/res/LongFormFunctionsHaveATerminatingReturnStatement.jl
+++ test/res/LongFormFunctionsHaveATerminatingReturnStatement.jl
@@ -33,6 +33,39 @@ function trailing_commentary(x::Int64)::Nothing
     end
 end                # It gets the correct end too, even if there's trailing whitespace and comments.
 
+function if_with_elseif_and_else_missing_if(x)::Int
+    if x == 1
+        println("some statement")
+        println("missing return")
+    elseif x == 2
+        return 2
+    else
+        return 3
+    end
+end  # Bad: missing return for `if x == 1`
+
+function if_with_elseif_and_else_missing_elseif(x)::Int
+    if x == 1
+        return 1
+    elseif x == 2
+        println("some statement")
+        println("missing return")
+    else
+        return 3
+    end
+end # Bad: missing return for `elseif x == 2`
+
+function if_with_elseif_and_else_missing_else(x)::Int
+    if x == 1
+        return 1
+    elseif x == 2
+        return 2
+    else
+        println("some statement")
+        println("missing return")
+    end
+end # Bad: missing return for `else`
+
 # Good style
 function foo()::String
     return "foo!"
@@ -44,3 +77,25 @@ function bar()::Nothing
 end
 
 short_hand() = "just to make sure"
+
+function if_with_else(x)::Int
+    if x == 1
+        return 1
+    else
+        return 2
+    end
+end
+
+function if_with_elseif_and_else(x)::Int
+    if x == 1
+        return 1
+    elseif x >= 2
+        if x % 2 == 0
+            return 2
+        else
+            return 3
+        end
+    else
+        return 4
+    end
+end
