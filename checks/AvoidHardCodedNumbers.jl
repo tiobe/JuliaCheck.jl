f9b356ccbf9d33fef9ea4c633e8fa8e9655f1910
zon-tiobe <paul.van.zon@tiobe.com>
2026-07-09 13:22:00 +0200

    Added clarifying message to AvoidHardCodedNumbers.

--- checks/AvoidHardCodedNumbers.jl
+++ checks/AvoidHardCodedNumbers.jl
@@ -31,11 +31,11 @@ const KNOWN_FLOATS = Set{Float64}([0.1, 0.01, 0.001, 0.0001, 0.5]) ∪
                     Set{Float64}(convert.(Float64, SOME_SPECIAL_INTS))
 
 struct Check<:Analysis.Check
-    seen_before::Set{Number}
+    seen_before::Dict{Number, Integer}
 
     # FIXME Fine for integers but, for floats, we should
     # probably use a tolerance to compare them.
-    Check() = new(Set{Number}())
+    Check() = new(Dict{Number, Integer}())
 end
 Analysis.id(::Check) = "avoid-hard-coded-numbers"
 Analysis.severity(::Check) = 3
@@ -51,10 +51,14 @@ function _check(this::Check, ctxt::AnalysisContext, node::SyntaxNode)::Nothing
     @assert is_literal_number(node) "Expected a node with a literal number, got $(kind(node))"
     if !_is_const_declaration(node) && !_in_array_assignment(node) && _is_magic_number(node)
         n = get_number(node)
-        if n ∈ this.seen_before
-            report_violation(ctxt, this, node, "Hard-coded number '$n' should be a const variable.")
+        if haskey(this.seen_before, n)
+            previous_line = this.seen_before[n]
+            current_line, _ = source_location(node)
+            extra_msg = previous_line == current_line ? "earlier on the same line" : "at line $previous_line"
+            report_violation(ctxt, this, node, "Hard-coded number '$n' (first used $extra_msg) should be a const variable.")
         else
-            push!(this.seen_before, n)
+            line, _ = source_location(node)
+            this.seen_before[n] = line
         end
     end
     return nothing
