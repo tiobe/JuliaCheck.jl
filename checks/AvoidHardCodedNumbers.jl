928baf04f6495d281a72fa2f1499aa15e68d5ac2
zon-tiobe <paul.van.zon@tiobe.com>
2026-07-21 11:52:05 +0200

    Addressed review remarks.

--- checks/AvoidHardCodedNumbers.jl
+++ checks/AvoidHardCodedNumbers.jl
@@ -1,5 +1,7 @@
 module AvoidHardCodedNumbers
 
+import JuliaSyntax: sourcetext
+
 using ...Properties: get_number, is_constant, is_global_decl, is_literal_number
 
 include("_common.jl")
@@ -31,11 +33,11 @@ const KNOWN_FLOATS = Set{Float64}([0.1, 0.01, 0.001, 0.0001, 0.5]) ∪
                     Set{Float64}(convert.(Float64, SOME_SPECIAL_INTS))
 
 struct Check<:Analysis.Check
-    number_occurrence::Dict{Number, Vector{SyntaxNode}}
+    number_occurrence::Dict{Number, SyntaxNode}
 
     # FIXME Fine for integers but, for floats, we should
     # probably use a tolerance to compare them.
-    Check() = new(Dict{Number, Vector{SyntaxNode}}())
+    Check() = new(Dict{Number, SyntaxNode}())
 end
 Analysis.id(::Check) = "avoid-hard-coded-numbers"
 Analysis.severity(::Check) = 3
@@ -52,14 +54,13 @@ function _check(this::Check, ctxt::AnalysisContext, node::SyntaxNode)::Nothing
     if !_is_const_declaration(node) && !_in_array_assignment(node) && _is_magic_number(node)
         n = get_number(node)
         if !haskey(this.number_occurrence, n)
-            this.number_occurrence[n] = Vector{SyntaxNode}()
-        end
-        push!(this.number_occurrence[n], node)
-        if length(this.number_occurrence[n]) > 1
-            first_instance_line, _ = source_location(first(this.number_occurrence[n]))
+            this.number_occurrence[n] = node
+        else
+            first_instance_line, _ = source_location(this.number_occurrence[n])
             current_instance_line, _ = source_location(node)
+            number_text = sourcetext(node)
             extra_msg = first_instance_line == current_instance_line ? "earlier on the same line" : "at line $first_instance_line"
-            report_violation(ctxt, this, node, "Hard-coded number '$n' (first used $extra_msg) should be a const variable.")
+            report_violation(ctxt, this, node, "Hard-coded number '$number_text' (first used $extra_msg) should be a const variable.")
         end
     end
     return nothing
