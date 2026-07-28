18fa2f7b35dcd1d772c168233bb42de33f55e2ea
reniers-tiobe <dennie.reniers@tiobe.com>
2026-07-28 15:18:04 +0200

    Use case-insensitive ordering for list of packages

--- checks/ModuleSingleImportLine.jl
+++ checks/ModuleSingleImportLine.jl
@@ -3,6 +3,8 @@ module ModuleSingleImportLine
 include("_common.jl")
 using ...Properties: get_imported_pkg, is_import, is_include, is_module
 
+using Unicode
+
 struct Check<:Analysis.Check end
 Analysis.id(::Check) = "module-single-import-line"
 Analysis.severity(::Check) = 9
@@ -22,8 +24,12 @@ function _check(this::Check, ctxt::AnalysisContext, module_node::SyntaxNode)::No
     imports = filter(is_import, children(children(module_node)[2]))
 
     _check_multiple_imports_on_line(this, ctxt, imports)
-    _check_import_ordering(this, ctxt, imports)
-    _check_include_ordering(this, ctxt, imports)
+
+    # Check imports/usings
+    _check_ordering(this, ctxt, imports, node -> !is_include(node) && numchildren(node) <= 1)
+
+    # Check includes
+    _check_ordering(this, ctxt, imports, is_include)
     return nothing
 end
 
@@ -36,32 +42,22 @@ function _check_multiple_imports_on_line(this::Check, ctxt::AnalysisContext, imp
     return nothing
 end
 
-function _check_import_ordering(this::Check, ctxt::AnalysisContext, imports::Vector{SyntaxNode})::Nothing
-    previous = ""
-    for node in filter(!is_include, imports)
-        pkg_name = get_imported_pkg(node)
-        if numchildren(node) <= 1
-            if pkg_name < previous
-                report_violation(ctxt, this, node, synopsis(this))
-                return nothing
-            else
-                previous = pkg_name
-            end
-        end
-    end
-    return nothing
-end
 
-function _check_include_ordering(this::Check, ctxt::AnalysisContext, imports::Vector{SyntaxNode})::Nothing
+function _check_ordering(
+    this::Check,
+    ctxt::AnalysisContext,
+    imports::Vector{SyntaxNode},
+    predicate::Function
+)::Nothing
     previous = ""
-    for node in filter(is_include, imports)
-        pkg_name = get_imported_pkg(node)
-        if pkg_name < previous
+    for node in filter(predicate, imports)
+        pkg_name_normalized = Unicode.normalize(get_imported_pkg(node), casefold=true)
+        if pkg_name_normalized < previous
             report_violation(ctxt, this, node, synopsis(this))
             return nothing
-        else
-            previous = pkg_name
         end
+
+        previous = pkg_name_normalized
     end
     return nothing
 end
