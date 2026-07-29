4e9d365739f5f62921e8a55b90a9d35fda19dd8f
reniers-tiobe <dennie.reniers@tiobe.com>
2026-07-29 09:19:59 +0200

    Reimplement rule by checking for array mutating operation

--- checks/AvoidCreatingEmptyArraysAndVectors.jl
+++ checks/AvoidCreatingEmptyArraysAndVectors.jl
@@ -1,9 +1,23 @@
 module AvoidCreatingEmptyArraysAndVectors
 
-using ...Properties: get_call_name_from_call_node, is_array_indx, is_assignment, is_call, is_vect
-using ...SymbolTable: node_is_declaration_of_variable
+using ...Properties: get_call_name_from_call_node, is_mutating_call
 
 include("_common.jl")
+
+const ARRAY_MUTATING_CALLS = Set([
+    "push!",
+    "pushfirst!",
+    "pop!",
+    "popfirst!",
+    "popat!",
+    "append!",
+    "prepend!",
+    "insert!",
+    "deleteat!",
+    "splice!",
+    "empty!",
+    "resize!",
+    ])
 
 struct Check<:Analysis.Check end
 Analysis.id(::Check) = "avoid-creating-empty-arrays-and-vectors"
@@ -11,56 +25,16 @@ Analysis.severity(::Check) = 8
 Analysis.synopsis(::Check) = "Avoid resizing arrays after initialization."
 
 function Analysis.init(this::Check, ctxt::AnalysisContext)::Nothing
-    register_syntaxnode_action(ctxt, is_assignment, n -> _check(this, ctxt, n))
+    register_syntaxnode_action(ctxt, is_mutating_call, n -> _check(this, ctxt, n))
     return nothing
 end
 
-function _check(this::Check, ctxt::AnalysisContext, assignment_node::SyntaxNode)::Nothing
-    if ! node_is_declaration_of_variable(ctxt.symboltable, first(children(assignment_node)))
-        return
-    end
-    assignment_value_node = last(children(assignment_node))
-    if _is_naive_empty_initialization(assignment_value_node) ||
-        _is_empty_keyword(assignment_value_node) ||
-        _is_empty_array_initialization(assignment_value_node)
-        if _has_sizehint(assignment_node)
-            return
-        end
-        report_violation(ctxt, this, assignment_node, "Avoid resizing arrays after initialization.")
+function _check(this::Check, ctxt::AnalysisContext, call_node::SyntaxNode)::Nothing
+    name = get_call_name_from_call_node(call_node)
+    if name ∈ ARRAY_MUTATING_CALLS
+        report_violation(ctxt, this, call_node, "Avoid resizing arrays after initialization.")
     end
     return nothing
-end
-
-function _has_sizehint(assignment_node::SyntaxNode)::Bool
-    assigned_variable = first(children(assignment_node)).data.val
-    sibling_nodes = children(assignment_node.parent)
-    for sibling_node in sibling_nodes
-        if is_call(sibling_node) && get_call_name_from_call_node(sibling_node) == "sizehint!"
-            var_node = children(sibling_node)[2]
-            if var_node.data.val == assigned_variable
-                return true
-            end
-        end
-    end
-    return false
-end
-
-function _is_naive_empty_initialization(node::SyntaxNode)::Bool
-    # A empty 'vect' kind SyntaxNode has two children; a 'ref' and a 'size'.
-    # Unlike what we might expect from a 'size' field in a child, this has nothing to do
-    # with the number of children; instead it seems to be a property.
-    return is_vect(node) && isnothing(node.data.val) && first(node.children.size) == 0
-end
-
-function _is_empty_keyword(node::SyntaxNode)::Bool
-    return is_call(node) && string(first(children(node))) == "empty"
-end
-
-# Single child means that there is nothing inside the array.
-# The parent node then looks like this: (= v (vect)). The "assignment value" , right-hand side
-# only has a single node. Any further children would define values inside the vector (or other type)
-function _is_empty_array_initialization(node::SyntaxNode)::Bool
-    return is_array_indx(node) && numchildren(node) == 1
 end
 
 end # end AvoidCreatingEmptyArraysAndVectors
